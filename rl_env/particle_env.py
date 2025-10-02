0db12c02a2f70b08c43b22d8852fe7ebccc9de79
Paula Hagen <paula@scadsdp25.misc.intern.uni-leipzig.de>
2025-10-02 10:10:14 +0200

    Changed window to be fixed and added constraint to actions so agents don't move outside the window

--- rl_env/particle_env.py
+++ rl_env/particle_env.py
@@ -1,12 +1,5 @@
 """
-# Simple
-
-```{figure} mpe_simple.gif
-:width: 140px
-:name: particle_env
-```
-
-This environment is part of the <a href='..'>MPE environments</a>. Please read that page first for general information.
+# Particle Environment
 
 | Import             |      `import particle_v1`      |
 |--------------------|----------------------------------------|
@@ -30,7 +23,7 @@ Observation space: `[self_vel, landmark_rel_position]`
 ### Arguments
 
 ``` python
-simple_v3.env(max_cycles=25, num_agents=10, num_food_sources=1, flow='none', continuous_actions = False, dynamic_rescaling=False)
+particle_v1.env(max_cycles=25, num_agents=10, num_food_sources=1, flow='none', continuous_actions = False, dynamic_rescaling=False)
 ```
 
 
@@ -56,6 +49,8 @@ from pettingzoo.utils.conversions import parallel_wrapper_fn
 from mpe2._mpe_utils.core import Agent, Landmark, World
 from mpe2._mpe_utils.scenario import BaseScenario
 from mpe2._mpe_utils.simple_env import SimpleEnv, make_env
+import pygame
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 class raw_env(SimpleEnv, EzPickle):
@@ -90,6 +85,112 @@ class raw_env(SimpleEnv, EzPickle):
             dynamic_rescaling=dynamic_rescaling,
         )
         self.metadata["name"] = "particle_v1"
+
+    # override draw function from MPE2 package 
+    def draw(self):
+        # clear screen
+        self.screen.fill((255, 255, 255))
+
+        # update bounds to center around agent
+        all_poses = [entity.state.p_pos for entity in self.world.entities]
+
+        # We want to prohibit agents moving outside the screen, so we choose a fixed cam_range
+        cam_range = self.original_cam_range
+
+        # The scaling factor is used for dynamic rescaling of the rendering - a.k.a Zoom In/Zoom Out effect
+        # The 0.9 is a factor to keep the entities from appearing "too" out-of-bounds
+        scaling_factor = 0.9 * cam_range
+
+        # update geometry and text positions
+        text_line = 0
+        for e, entity in enumerate(self.world.entities):
+            # geometry
+            x, y = entity.state.p_pos
+            y *= (
+                -1
+            )  # this makes the display mimic the old pyglet setup (ie. flips image)
+            x = (
+                (x / cam_range) * self.width // 2 * 0.9
+            )  # the .9 is just to keep entities from appearing "too" out-of-bounds
+            y = (y / cam_range) * self.height // 2 * 0.9
+            x += self.width // 2
+            y += self.height // 2
+
+            # 350 is an arbitrary scale factor to get pygame to render similar sizes as pyglet
+            if self.dynamic_rescaling:
+                radius = entity.size * 350 * scaling_factor
+            else:
+                radius = entity.size * 350
+
+            pygame.draw.circle(self.screen, entity.color * 200, (x, y), radius)
+            pygame.draw.circle(self.screen, (0, 0, 0), (x, y), radius, 1)  # borders
+            assert (
+                0 < x < self.width and 0 < y < self.height
+            ), f"Coordinates {(x, y)} are out of bounds."
+
+            # text
+            if isinstance(entity, Agent):
+                if entity.silent:
+                    continue
+                if np.all(entity.state.c == 0):
+                    word = "_"
+                elif self.continuous_actions:
+                    word = (
+                        "[" + ",".join([f"{comm:.2f}" for comm in entity.state.c]) + "]"
+                    )
+                else:
+                    word = alphabet[np.argmax(entity.state.c)]
+
+                message = entity.name + " sends " + word + "   "
+                message_x_pos = self.width * 0.05
+                message_y_pos = self.height * 0.95 - (self.height * 0.05 * text_line)
+                self.game_font.render_to(
+                    self.screen, (message_x_pos, message_y_pos), message, (0, 0, 0)
+                )
+                text_line += 1
+    
+    # override _execute_world_step function
+    def _execute_world_step(self):
+        # set action for each agent
+        for i, agent in enumerate(self.world.agents):
+            action = self.current_actions[i]
+            scenario_action = []
+            if agent.movable:
+                mdim = self.world.dim_p * 2 + 1
+                if self.continuous_actions:
+                    scenario_action.append(action[0:mdim])
+                    action = action[mdim:]
+                else:
+                    scenario_action.append(action % mdim)
+                    action //= mdim
+            if not agent.silent:
+                scenario_action.append(action)
+            self._set_action(scenario_action, agent, self.action_spaces[agent.name])
+
+        self.world.step()
+        # after action is chosen, clip the positions of all entities to stay in bounds
+        for entity in self.world.entities:
+            entity.state.p_pos = np.clip(
+                entity.state.p_pos,
+                -self.original_cam_range,
+                self.original_cam_range
+            )
+
+        global_reward = 0.0
+        if self.local_ratio is not None:
+            global_reward = float(self.scenario.global_reward(self.world))
+
+        for agent in self.world.agents:
+            agent_reward = float(self.scenario.reward(agent, self.world))
+            if self.local_ratio is not None:
+                reward = (
+                    global_reward * (1 - self.local_ratio)
+                    + agent_reward * self.local_ratio
+                )
+            else:
+                reward = agent_reward
+
+            self.rewards[agent.name] = reward
 
 
 env = make_env(raw_env)
