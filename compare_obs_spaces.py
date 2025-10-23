dfbc6c3c91872151385e7442a1315be332eb1a28
Paula Hagen <paula@scadsdp25.misc.intern.uni-leipzig.de>
2025-10-23 14:06:05 +0200

    Added type of observation space (god, vision, proprioception) to compare. Tried to fix order in environment step because agents still overlap in image (but not successful).

---
+++ compare_obs_spaces.py
@@ -0,0 +1,144 @@
+"""
+Based on file from Elliot Tower from Pettingzoo (https://github.com/elliottower), which can be found here: https://pettingzoo.farama.org/tutorials/sb3/kaz/
+"""
+import glob
+import os
+import time
+import multiprocessing
+import argparse
+
+import supersuit as ss
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import VecMonitor
+
+import particle_v1
+
+def train(env_fn, steps: int = 300000, seed: int | None = 0, render_mode: str | None = None, **env_kwargs):
+    # Load environment and preprocess using supersuit
+    env = env_fn.parallel_env(render_mode=render_mode, **env_kwargs)
+    print(f"Starting training on {str(env.metadata['name'])} with \"{OBS_TYPE}\" observation space.")
+    env.reset(seed=seed) 
+
+    # Stack frames, so context of actions is included (helpful for learning movement)
+    env = ss.frame_stack_v1(env, stack_size=4)
+
+    # This is crucial for combining multi-agent observations
+    env = ss.multiagent_wrappers.pad_observations_v0(env)
+
+    # Convert parallel to Markov VectorEnv (needed for SB3). This treats each agent as a separate environment, so if num_agents=10, this is like having 10 envs.
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+
+    # Concatenate multiple environments for SB3 -> SB3VecEnvWrapper. We can now train on several vector environments in parallel (per agent).
+    # num_vec_envs is per agent, so in total we train on num_vec_envs*num_agents environments --> should be a multiple of num_cpus for best performance.
+    env = ss.concat_vec_envs_v1(vec_env=env, num_vec_envs=1, num_cpus=NUM_CORES, base_class="stable_baselines3") 
+
+    # This helps tensorboard logging
+    env = VecMonitor(env, filename=(LOG_PATH + '/PPO_' + OBS_TYPE))
+
+    # Initiate PPO model depending on observation space type. (Default "god")
+    if OBS_TYPE == 'god':
+        mycode = "enter code"
+    elif OBS_TYPE == 'vision':
+        mycode = "enter code"
+    elif OBS_TYPE == 'proprioception':
+        mycode = "enter code"
+    model = PPO('MlpPolicy', env, verbose=3, tensorboard_log = LOG_PATH)
+    
+    # Train and save model
+    model.learn(total_timesteps=steps)
+    model.save(f"models/PPO_{OBS_TYPE}_{time.strftime('%Y%m%d-%H%M%S')}")
+    print("Model has been saved.")
+    print(f"Finished training on {str(env.unwrapped.metadata['name'])}.")
+
+    env.close()
+
+
+def eval(env_fn, num_games: int = 100, render_mode: str | None = None, **env_kwargs):
+    # Evaluate a trained agent vs a random agent
+    env = env_fn.env(render_mode = render_mode, **env_kwargs)
+    env = ss.frame_stack_v1(env, stack_size=4)
+
+    print(
+        f"\nStarting evaluation on {str(env.metadata['name'])} (num_games={num_games}, render_mode={render_mode})"
+    )
+
+    try:
+        latest_policy = max(
+            glob.glob(f"models/PPO_{OBS_TYPE}*.zip"), key=os.path.getctime
+        )
+    except ValueError:
+        print("Policy not found.")
+        exit(0)
+
+    # Load saved model
+    model = PPO.load(latest_policy)
+    
+    rewards = {agent: 0 for agent in env.possible_agents}
+
+    # Note: we evaluate here using an AEC environments, to allow for easy A/B testing against random policies
+    # For example, we can see here that using a random agent for archer_0 results in less points than the trained agent
+    for i in range(num_games):
+        env.reset(seed=i)
+        env.action_space(env.possible_agents[0]).seed(i)
+
+        for agent in env.agent_iter():
+            obs, reward, termination, truncation, info = env.last()
+
+            for a in env.agents:
+                rewards[a] += env.rewards[a]
+
+            if termination or truncation:
+                break
+            else:
+                if agent == env.possible_agents[0]:
+                    act = env.action_space(agent).sample()
+                else:
+                    act = model.predict(obs, deterministic=True)[0]
+            env.step(act)
+    env.close()
+
+    avg_reward = sum(rewards.values()) / len(rewards.values())
+    avg_reward_per_agent = {
+        agent: rewards[agent] / num_games for agent in env.possible_agents
+    }
+    print(f"Avg reward: {avg_reward}")
+    print("Avg reward per agent, per game: ", avg_reward_per_agent)
+    print("Full rewards: ", rewards)
+    return avg_reward
+
+
+if __name__ == "__main__":
+    # Needed on MacOS(according to supersuit doc)
+    multiprocessing.set_start_method("fork")
+    
+    # Make tensorboard log dir
+    os.makedirs("logs/", exist_ok=True)
+    LOG_PATH = "logs/"
+
+    # Get number of cpus
+    NUM_CORES = multiprocessing.cpu_count()
+
+    # Get env function
+    env_fn = particle_v1
+    
+    # Get model type through parser
+    parser = argparse.ArgumentParser(description="Enter type of observation space.")
+    parser.add_argument('obs_type', help='Type of observation space for agents (god, vision, proprioception)')
+    args = parser.parse_args()
+
+    if args.obs_type in ['god', 'vision', 'proprioception']:
+        OBS_TYPE = args.obs_type
+    else:
+        print('No correct observation space type given. Options are: god, vision, proprioception.')
+
+    # Set vector_state to false in order to use visual observations (significantly longer training time)
+    env_kwargs = dict(num_agents=10, num_food_sources=1, obs_type = OBS_TYPE, flow='none', max_cycles=100)
+ 
+    # Train a model
+    train(env_fn, steps=1000000, seed=0, render_mode = None, **env_kwargs)
+
+    # Evaluate 10 games without rendering
+    #eval(env_fn, num_games=10, render_mode=None, **env_kwargs)
+
+    # Watch 2 games in pygame window
+    #eval(env_fn, num_games=2, render_mode="human", **env_kwargs)
--- rl_env/particle_env.py
+++ rl_env/particle_env.py
@@ -59,6 +59,7 @@ class raw_env(SimpleEnv, EzPickle):
         self,
         num_agents=10,
         num_food_sources=1,
+        obs_type = 'god',
         flow = 'none',
         max_cycles=25,
         continuous_actions=False,
@@ -75,7 +76,7 @@ class raw_env(SimpleEnv, EzPickle):
             render_mode=render_mode,
         )
         scenario = Scenario()
-        world = scenario.make_world(num_agents, num_food_sources, flow)
+        world = scenario.make_world(num_agents, num_food_sources, obs_type, flow)
         SimpleEnv.__init__(
             self,
             scenario=scenario,
@@ -116,7 +117,7 @@ class raw_env(SimpleEnv, EzPickle):
         entities_flipped = self.world.entities[::-1]
 
         # update bounds to center around agent
-        all_poses = [entity.state.p_pos for entity in entities_flipped]
+        #all_poses = [entity.state.p_pos for entity in entities_flipped]
 
         # We want to prohibit agents moving outside the screen, so we choose a fixed cam_range
         cam_range = self.original_cam_range
@@ -192,6 +193,9 @@ class raw_env(SimpleEnv, EzPickle):
             self._set_action(scenario_action, agent, self.action_spaces[agent.name])
 
         self.world.step()
+        #print("step is done")
+        #print(" ")
+        #print(" ")
         # after action is chosen, clip the positions of all entities to stay in bounds
         for entity in self.world.entities:
             entity.state.p_pos = np.clip(
@@ -221,10 +225,11 @@ env = make_env(raw_env) # this throws an error for the return of the overwritten
 parallel_env = parallel_wrapper_fn(env)
 
 class Scenario(BaseScenario):
-    def make_world(self, num_agents=10, num_food_sources=1, flow='none'):
+    def make_world(self, num_agents=10, num_food_sources=1, obs_type = 'god', flow='none'):
         world = MyWorld()
         num_agents = num_agents
         num_food_sources = num_food_sources
+        world.obs_type = obs_type
         # add agents
         world.agents = [Agent() for i in range(num_agents)]
         for i, agent in enumerate(world.agents):
@@ -242,13 +247,24 @@ class Scenario(BaseScenario):
             landmark.collide = False
             landmark.movable = False
             landmark.size = 0.2
-            landmark.boundary = False # If True, landmark cannot be seen in observations
+            landmark.boundary = True # If True, landmark cannot be seen in observations
         return world
 
     def reset_world(self, world, np_random):
         # greenish colour for agents
+        colors = [np.array([0.35, 0.85, 0.35]),
+                  np.array([0.85, 0.0, 0.35]),
+                  np.array([0.35, 0.35, 0.35]), 
+                  np.array([0.0, 0.0, 0.35]), 
+                  np.array([0.9, 0.85, 0.2]),
+                  np.array([0.2, 0.85, 0.9]),
+                  np.array([0.7, 0.9, 0.9]),
+                  np.array([0.1, 0.85, 0.35]),
+                  np.array([0.2, 0.0, 0.35]),
+                  np.array([0.6, 0.85, 0.0])]
         for i, agent in enumerate(world.agents):
-            agent.color = np.array([0.35, 0.85, 0.35])
+            #agent.color = np.array([0.35, 0.85, 0.35])
+            agent.color = colors[i]
         # orange colour for landmarks
         for i, landmark in enumerate(world.landmarks):
             landmark.color = np.array([0.95, 0.45, 0.15])
@@ -281,7 +297,7 @@ class Scenario(BaseScenario):
     # (2) no collisions with other agents
     # (3) staying within the environment bounds
     def reward(self, agent, world):
-        dist_to_food = np.sum(np.square(agent.state.p_pos - world.landmarks[0].state.p_pos))
+        dist_to_food = np.sqrt(np.sum(np.square(agent.state.p_pos - world.landmarks[0].state.p_pos)))
         reward_for_not_colliding = 0.0
         if agent.collide:
             for a in world.agents:
@@ -293,18 +309,26 @@ class Scenario(BaseScenario):
         return -dist_to_food + border_position_penalty # + reward_for_not_colliding # Add after size problems and collision bug are fixed 
 
     def observation(self, agent, world):
-        # get positions of all entities in this agent's reference frame
+        # get distances to all other entities for this agent and combine with its velocity
         entity_pos = []
-        for entity in world.landmarks:
-            entity_pos.append(entity.state.p_pos - agent.state.p_pos)
-        return np.concatenate([agent.state.p_vel] + entity_pos) # entity_pos are the vectors to the other entities
+        if world.obs_type == "god":
+            for entity in world.entities:
+                entity_pos.append(entity.state.p_pos) # entity_pos are the vectors to the other entities
+        elif world.obs_type == "vision":
+            for entity in world.entities:
+                entity_pos.append(entity.state.p_pos - agent.state.p_pos) # old structure right now
+        elif world.obs_type == "proprioception":
+            for entity in world.entities:
+                if entity == agent:
+                    entity_pos.append(entity.state.p_pos) # only own position added
+                else:
+                    entity_pos.append(np.array([0.0,0.0]))
+        else:
+            print("Incorrect observation space type. Enter \"god\", \"vision\" or \"proprioception\"")
+
+        return np.concatenate([agent.state.p_vel] + entity_pos) # adds vel at the front of the list (no math. operations)
 
 class MyWorld(World):
-    # Override the function that returns all entities in the world
-    @property
-    def entities(self):
-        return self.agents + self.landmarks
-    
     # Override apply_action_force function from _mpe2_utils/core.py
     def apply_action_force(self, p_force):
         # set applied forces
@@ -321,6 +345,94 @@ class MyWorld(World):
                 p_force[i] = agent.action.u + noise # Brownian vector is added to the action vector
         return p_force
     
+    # Override step function from _mpe2/utils/core.py
+    # update state of the world
+    def step(self):
+        # set actions for scripted agents
+        for agent in self.scripted_agents:
+            agent.action = agent.action_callback(agent, self)
+        # gather forces applied to entities
+        p_force = [None] * len(self.entities)
+        # apply agent physical controls
+        p_force = self.apply_action_force(p_force)
+
+        # apply environment forces
+        '''
+        collision_detected = True
+        i = 0
+        while collision_detected:
+            print("while loop started")
+            p_force = self.apply_environment_force(p_force)
+            # integrate physical state
+            self.integrate_state(p_force)
+            # update agent state
+            for agent in self.agents:
+                self.update_agent_state(agent)
+            if self.is_any_collision():
+                collision_detected = True
+                print("Collision detected!")
+            else: 
+                False
+            print("while loop ended")
+            i +=1
+            if i > 10:
+                collision_detected = False
+            print('------------------')
+        '''
+        p_force = self.apply_environment_force(p_force)
+        # integrate physical state
+        self.integrate_state(p_force)
+        # update agent state
+        for agent in self.agents:
+            self.update_agent_state(agent)
+
+    # Override apply_environment_force function from _mpe2_utils/core.py to account for multiple collision (new collision after corrected course)
+    # gather physical forces acting on entities
+    def apply_environment_force(self, p_force): 
+        for a, entity_a in enumerate(self.entities):
+            for b, entity_b in enumerate(self.entities):
+                if b <= a:
+                    continue
+                [f_a, f_b] = self.get_collision_force(entity_a, entity_b)
+                if f_a is not None or f_b is not None:
+                    if p_force[a] is None:
+                        p_force[a] = 0.0
+                    p_force[a] = f_a + p_force[a]
+                    if p_force[b] is None:
+                        p_force[b] = 0.0
+                    p_force[b] = f_b + p_force[b]
+        return p_force
+    
+    # Override integrate_state function from _mpe2_utils/core.py
+    # integrate physical state
+    def integrate_state(self, p_force):
+        #print('new_round')
+        #print('#################')
+        for i, entity in enumerate(self.entities):
+            if not entity.movable:
+                continue
+            #entity.state.p_pos += entity.state.p_vel * self.dt
+            #print('Old pos: ', entity.state.p_pos)
+            entity.state.p_vel = entity.state.p_vel * (1 - self.damping) # damping is loss of energy, here 0.25
+            if p_force[i] is not None:
+                entity.state.p_vel += (p_force[i] / entity.mass) * self.dt # mass is 1 and dt is 0.1 (time step interval)
+            if entity.max_speed is not None:
+                speed = np.sqrt(
+                    np.square(entity.state.p_vel[0]) + np.square(entity.state.p_vel[1])
+                )
+                if speed > entity.max_speed:
+                    entity.state.p_vel = (
+                        entity.state.p_vel
+                        / np.sqrt(
+                            np.square(entity.state.p_vel[0])
+                            + np.square(entity.state.p_vel[1])
+                        )
+                        * entity.max_speed
+                    )
+            entity.state.p_pos += entity.state.p_vel * self.dt
+            #print('New pos: ', entity.state.p_pos)
+            #print('--------------------')
+    
     # Override get_collision_force function from _mpe2_utils/core.py (to prevent from dividing by zero)
     # get collision forces for any contact between two entities
     def get_collision_force(self, entity_a, entity_b):
@@ -330,9 +442,12 @@ class MyWorld(World):
             return [None, None]  # don't collide against itself
         # compute actual distance between entities
         delta_pos = entity_a.state.p_pos - entity_b.state.p_pos
+        # compute Euclidean distance
         dist = np.sqrt(np.sum(np.square(delta_pos)))
         # minimum allowable distance
         dist_min = entity_a.size + entity_b.size
+        if dist >= dist_min: # agents are far enough away from each other
+            return [None, None]
         # softmax penetration
         k = self.contact_margin
         penetration = np.logaddexp(0, -(dist - dist_min) / k) * k
@@ -342,3 +457,17 @@ class MyWorld(World):
         force_a = +force if entity_a.movable else None
         force_b = -force if entity_b.movable else None
         return [force_a, force_b]
+    
+    # Function from simple spread environment to check for collisions
+    def is_any_collision(self):
+        collision = False
+        for a, agent_a in enumerate(self.entities):
+            for b, agent_b in enumerate(self.entities):
+                if b <= a:
+                    continue
+                delta_pos = agent_a.state.p_pos - agent_b.state.p_pos
+                dist = np.sqrt(np.sum(np.square(delta_pos)))
+                dist_min = agent_a.size + agent_b.size
+                if dist < dist_min:
+                    collision = True
+        return collision
